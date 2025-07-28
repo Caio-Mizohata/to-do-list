f5bd35c7a1605a329e5593b04850e16951e2f626
caioeduardo4100 <Caiomizohata@gmail.com>
2025-07-27 21:27:35 -0300

    Admin added

--- to_do/admin.py
+++ to_do/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from to_do.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed', 'created_at', 'updated_at')
+    list_filter = ('completed', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
+
+admin.site.register(Task)
